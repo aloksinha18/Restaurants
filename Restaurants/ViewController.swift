49d059a59f9cbef3ccad2e009a9a7087a505228b
Alok <iosdevnative@gmail.com>
2022-06-26 23:36:27 +0200

    Initial Commit

---
+++ Restaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurants
+//
+//  Created by Alok Sinha on 2022-06-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
