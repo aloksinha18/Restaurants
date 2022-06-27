1bebbf4c876de43303067e4925443a4fa3a915dd
Alok <iosdevnative@gmail.com>
2022-06-27 23:33:29 +0200

    update `RestaurantsListViewModel` by injecting ` RestaurantLoader` and fetch restaurants using loader

--- Restaurants/ViewModel/RestaurantsListViewModel.swift
+++ Restaurants/ViewModel/RestaurantsListViewModel.swift
@@ -9,22 +9,43 @@ import Foundation
 
 class RestaurantsListViewModel {
     
-    private var restaurants: [Restaurant]
+    private let loader: RestaurantLoader
+    private var restaurants: [Restaurant] = []
     
-    init(restaurants: [Restaurant]) {
-        self.restaurants = restaurants
+    var onLoad: (([Restaurant])-> Void)?
+    var onFail: ((Error)-> Void)?
+    
+    init(loader: RestaurantLoader) {
+        self.loader = loader
     }
     
-    var sortedRestaurants: [Restaurant] {
-        sortRestaurantsByStatus(restaurants)
+    func load() {
+        loader.load { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let restaurants):
+                let sortedList = sortRestaurantsByStatus(restaurants)
+                self.restaurants = sortedList
+                onLoad?(sortedList)
+            case .failure(let error):
+                onFail?(error)
+            }
+        }
     }
     
-    private func sortRestaurantsByStatus(_ restaurant: [Restaurant]) -> [Restaurant] {
-        restaurants = restaurant.sorted(by: { $0.status > $1.status })
+    private func sortRestaurantsByStatus(_ restaurants: [Restaurant]) -> [Restaurant] {
+        let restaurants = restaurants.sorted(by: { $0.status > $1.status })
         return restaurants
     }
     
     func sortedRestaurantsByName(_ input: String)  {
-        restaurants = restaurants.filter { $0.name.localizedCaseInsensitiveContains(input) }
+        let restaurants = restaurants.filter { $0.name.localizedCaseInsensitiveContains(input) }
+        onLoad?(restaurants)
+    }
+    
+    func removeSearch() {
+        let restaurants = restaurants.sorted(by: { $0.status > $1.status })
+        self.restaurants = restaurants
+        onLoad?(restaurants)
     }
 }
--- RestaurantsTests/ViewModel/RestaurantsListViewModelTests.swift
+++ RestaurantsTests/ViewModel/RestaurantsListViewModelTests.swift
@@ -8,19 +8,41 @@
 import XCTest
 @testable import Restaurants
 
+class MockRestaurantsLoader: RestaurantLoader {
+    private let result: Result<[Restaurant], Error>
+    
+    init(result: Result<[Restaurant], Error>) {
+        self.result = result
+    }
+        
+    func load(completion: (Result<[Restaurant], Error>) -> Void) {
+        completion(result)
+    }
+}
+
 final class RestaurantsListViewModelTests: XCTestCase {
     
     func test_load_in_sorted_order() {
-        
         let firstRestaurant = getRestaurant(with: "First Restaurant", status: .closed)
         let secondRestaurant = getRestaurant(with: "Second Restaurant", status: .statusOpen)
         let thirdRestaurant = getRestaurant(with: "Third Restaurant", status: .orderAhead)
-        
-        let sut = RestaurantsListViewModel(restaurants: [firstRestaurant, secondRestaurant, thirdRestaurant])
-        
-        XCTAssertEqual(sut.sortedRestaurants[0].status, .statusOpen)
-        XCTAssertEqual(sut.sortedRestaurants[1].status, .orderAhead)
-        XCTAssertEqual(sut.sortedRestaurants[2].status, .closed)
+        let loader = MockRestaurantsLoader(result: .success([firstRestaurant, secondRestaurant, thirdRestaurant]))
+        let sut = RestaurantsListViewModel(loader: loader)
+        sut.load()
+        sut.onLoad = { result in
+            XCTAssertEqual(result[0].status, .statusOpen)
+            XCTAssertEqual(result[1].status, .orderAhead)
+            XCTAssertEqual(result[2].status, .closed)
+        }
+    }
+    
+    func test_load_fails() {
+        let loader = MockRestaurantsLoader(result: .failure(anyNSError()))
+        let sut = RestaurantsListViewModel(loader: loader)
+        sut.load()
+        sut.onFail = { error in
+            XCTAssertNotNil(error)
+        }
     }
     
     func test_load_in_sorted_order_after_search() {
@@ -29,16 +51,45 @@ final class RestaurantsListViewModelTests: XCTestCase {
         let firstOrderaheadRestaurant = getRestaurant(with: "First Restaurant", status: .statusOpen)
         let secondRestaurant = getRestaurant(with: "Second Restaurant", status: .statusOpen)
         let thirdRestaurant = getRestaurant(with: "Third Restaurant", status: .orderAhead)
-        let sut = RestaurantsListViewModel(restaurants: [firstClosedRestaurant,firstOpenRestaurant,firstOrderaheadRestaurant, secondRestaurant, thirdRestaurant])
         
+        let sut = RestaurantsListViewModel(loader: MockRestaurantsLoader(result: .success([firstClosedRestaurant,firstOpenRestaurant,firstOrderaheadRestaurant, secondRestaurant, thirdRestaurant])))
+
         sut.sortedRestaurantsByName("First")
-        XCTAssertEqual(sut.sortedRestaurants.count, 3)
-        XCTAssertEqual(sut.sortedRestaurants[0].status, .statusOpen)
-        XCTAssertEqual(sut.sortedRestaurants[1].status, .orderAhead)
-        XCTAssertEqual(sut.sortedRestaurants[2].status, .closed)
+        
+        sut.onLoad = { result in
+            XCTAssertEqual(result.count, 3)
+            XCTAssertEqual(result[0].status, .statusOpen)
+            XCTAssertEqual(result[1].status, .orderAhead)
+            XCTAssertEqual(result[2].status, .closed)
+        }
+    }
+    
+    func test_resetting_restaurants_after_removing_search() {
+        let firstClosedRestaurant = getRestaurant(with: "First Restaurant", status: .closed)
+        let firstOpenRestaurant = getRestaurant(with: "First Restaurant", status: .orderAhead)
+        let firstOrderaheadRestaurant = getRestaurant(with: "First Restaurant", status: .statusOpen)
+        let secondRestaurant = getRestaurant(with: "Second Restaurant", status: .statusOpen)
+        let thirdRestaurant = getRestaurant(with: "Third Restaurant", status: .orderAhead)
+        
+        let sut = RestaurantsListViewModel(loader: MockRestaurantsLoader(result: .success([firstClosedRestaurant,firstOpenRestaurant,firstOrderaheadRestaurant, secondRestaurant, thirdRestaurant])))
+
+        sut.sortedRestaurantsByName("First")
+        sut.removeSearch()
+        sut.onLoad = { result in
+            XCTAssertEqual(result.count, 5)
+            XCTAssertEqual(result[0].status, .statusOpen)
+            XCTAssertEqual(result[1].status, .statusOpen)
+            XCTAssertEqual(result[2].status, .orderAhead)
+            XCTAssertEqual(result[3].status, .orderAhead)
+            XCTAssertEqual(result[4].status, .closed)
+        }
     }
     
     private func getRestaurant(with name: String, status: Status)-> Restaurant {
         Restaurant(name: name, status: status, sortingValues: SortingValues(bestMatch: 0.0, newest: 96.0, ratingAverage: 4.5, distance: 1190, popularity: 17, averageProductPrice: 1536, deliveryCosts: 200, minCost: 1000))
+    }
+    
+    private func anyNSError() -> NSError {
+        NSError(domain: "Test", code: 404)
     }
 }
