d519149a419895db143f68effffe54d1186f8190
Alok <iosdevnative@gmail.com>
2022-06-27 23:47:52 +0200

    add  `RestaurantsViewController`

---
+++ Restaurants/View/RestaurantsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RestaurantsViewController.swift
+//  Restaurants
+//
+//  Created by Alok Sinha on 2022-06-27.
+//
+
+import UIKit
+
+final class RestaurantsViewController: UITableViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+}
